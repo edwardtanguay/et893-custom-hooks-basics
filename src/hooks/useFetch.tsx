202e462acbff24715ca301e3ec9b6aa79fcf9d59
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-04 11:12:18 +0100

    usefetch with ts

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 
-export const useFetch = (url: string) => {
-	const [items, setItems] = useState([]);
+export const useFetch = <T,>(url: string) => {
+	const [items, setItems] = useState<T>([] as T);
 
 	useEffect(() => {
 		(async () => {
--- src/pages/PageFetch.tsx
+++ src/pages/PageFetch.tsx
@@ -1,9 +1,21 @@
 import { useFetch } from "../hooks/useFetch";
+import { ISkill } from "../interfaces";
 
 export const PageFetch = () => {
-	const { items: skills } = useFetch(
+	const { items: skills } = useFetch<ISkill[]>(
 		"https://edwardtanguay.vercel.app/share/skills_with_id.json"
 	);
 
-	return <p>There are {skills.length} skills.</p>;
+	return (
+		<div>
+			<p>There are {skills.length} skills.</p>
+			<p className="text-xs">
+				{skills
+					.map((skill) => {
+						return skill.name;
+					})
+					.join(", ")}
+			</p>
+		</div>
+	);
 };
