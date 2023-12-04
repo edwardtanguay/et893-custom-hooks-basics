6f26e6a2bb772b0120424c0ac0733b5f1b25a015
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-04 11:57:09 +0100

    localstorage works

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 
-export const useLocalStorage = (defaultValue = '') => {
+export const useLocalStorage = (idCode: string, defaultValue = "") => {
 	const [value, setValue] = useState(defaultValue);
 
+	useEffect(() => {
+		const _value = localStorage.getItem(idCode);
+		if (_value !== null) {
+			setValue(_value);
+		}
+	}, []);
+
 	const save = (_value: string) => {
-		setValue(_value)
-	}
+		setValue(_value);
+		localStorage.setItem(idCode, _value);
+	};
 
 	return { value, save };
-}
+};
--- src/pages/PageLocalStorage.tsx
+++ src/pages/PageLocalStorage.tsx
@@ -1,7 +1,7 @@
 import { useLocalStorage } from "../hooks/useLocalStorage"
 
 export const PageLocalStorage = () => {
-	const { value: title, save: saveTitle } = useLocalStorage('nnn');
+	const { value: title, save: saveTitle } = useLocalStorage('title', '');
 
 	return (
 		<p>Title = <input value={title} onChange={(e) => saveTitle(e.target.value)} /> ({title})</p>
