c77ad39a7eb82bed1171b59f115dd650308dc239
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-04 11:34:42 +0100

    useFetchs works for jobs/skills

--- src/pages/PageFetch.tsx
+++ src/pages/PageFetch.tsx
@@ -1,27 +1,40 @@
 import { useFetch } from "../hooks/useFetch";
-import { ISkill } from "../interfaces";
+import { IJob, ISkill } from "../interfaces";
 
 export const PageFetch = () => {
-	const { items: skills, isLoading } = useFetch<ISkill[]>(
-		"https://edwardtanguay.vercel.app/share/skills_with_id.json", true
+	const { items: skills, isLoading: isLoadingSkills } = useFetch<ISkill[]>(
+		"https://edwardtanguay.vercel.app/share/skills_with_id.json",
+		true
+	);
+	const { items: jobs, isLoading: isLoadingJobs } = useFetch<IJob[]>(
+		"https://edwardtanguay.vercel.app/share/jobs.json", true
 	);
 
 	return (
-		<div>
-			{isLoading ? (
-				<p>Loading skills...</p>
-			) : (
-			<>
-			<p>There are {skills.length} skills.</p>
-			<p className="text-xs">
-				{skills
-					.map((skill) => {
-						return skill.name;
-					})
-					.join(", ")}
-			</p>
-			</>
-			)}
-		</div>
+		<>
+			<div className="mb-4">
+				{isLoadingSkills ? (
+					<p>Loading skills...</p>
+				) : (
+					<>
+						<p>There are {skills.length} skills.</p>
+						<p className="text-xs">
+							{skills
+								.map((skill) => {
+									return skill.name;
+								})
+								.join(", ")}
+						</p>
+					</>
+				)}
+			</div>
+			<div>
+				{isLoadingJobs ? (
+					<p>Loading jobs...</p>
+				) : (
+					<p>There are {jobs.length} jobs.</p>
+				)}
+			</div>
+		</>
 	);
 };
